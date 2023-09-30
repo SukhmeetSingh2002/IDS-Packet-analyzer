a8d9bff5b333daefbf4469170f17875e1d5a5c94
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-09-30 16:09:59 +0530

    add scrollbar

--- Assignment_1/GUI_interface.py
+++ Assignment_1/GUI_interface.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from Interface import Interface
 
 from scapy.utils import RawPcapReader
@@ -8,6 +8,7 @@ from ip_packet_identifier.src.pcap_processor import process_file
 import customtkinter
 import tkinter as tk
 from tkinter import ttk
+
 
 
 class GUIInterface(Interface):
@@ -27,7 +28,11 @@ class GUIInterface(Interface):
         self.file_label = tk.Label(self.root, text="No file selected.")
         self.file_button = tk.Button(self.root, text="Select file", command=self.select_file)
         self.process_button = tk.Button(self.root, text="Process file", command=self.process_file)
-        self.stats_tree = ttk.Treeview(self.root, columns=("Packet #", "IP Version", "Source Address", "Destination Address", "Protocol", "Source Port", "Destination Port", "Flags", "ICMP Type", "ICMP Code"))
+        self.stats_tree = ttk.Treeview(self.root, columns=("Packet #", "IP Version", "Source Address", "Destination Address", "Protocol", "Source Port", "Destination Port", "Flags", "ICMP Type", "ICMP Code", "UDP Source Port", "UDP Destination Port"))
+        self.stats_scrollbar = ttk.Scrollbar(self.root, orient="vertical", command=self.stats_tree.yview)
+        # self.stats_tree.configure(yscrollcommand=self.stats_scrollbar.set)
+        self.stats_hscrollbar = ttk.Scrollbar(self.root, orient="horizontal", command=self.stats_tree.xview)
+        # self.stats_tree.configure(xscrollcommand=self.stats_hscrollbar.set)
 
         # Configure treeview columns
         self.stats_tree.heading("#0", text="Packet #")
@@ -40,12 +45,23 @@ class GUIInterface(Interface):
         self.stats_tree.heading("#7", text="Flags")
         self.stats_tree.heading("#8", text="ICMP Type")
         self.stats_tree.heading("#9", text="ICMP Code")
+        self.stats_tree.heading("#10", text="UDP Source Port")
+        self.stats_tree.heading("#11", text="UDP Destination Port")
+        
+        # Configure treeview scrollbars
+        self.stats_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.stats_hscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.stats_tree.configure(yscrollcommand=self.stats_scrollbar.set, xscrollcommand=self.stats_hscrollbar.set)
+        # self.stats_scrollbar.configure(command=self.stats_tree.yview)
+        # self.stats_hscrollbar.configure(command=self.stats_tree.xview)
 
         # Layout widgets
         self.file_label.pack()
         self.file_button.pack()
         self.process_button.pack()
         self.stats_tree.pack()
+        self.stats_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     def select_file(self):
         """Open a file dialog box to select a pcap file."""
@@ -65,7 +81,7 @@ class GUIInterface(Interface):
     def process_file(self, *args):
         """Process the selected pcap file and display the statistics."""
         if self.file_name is None:
-            tk.messagebox.showerror("Error", "No file selected.")
+            messagebox.showerror("Error", "No file selected.")
             return
 
         stats = self.my_process_file(self.file_name)
@@ -85,16 +101,18 @@ class GUIInterface(Interface):
                 packet_stats["ip"]["embedded_protocol"],
                 packet_stats.get("tcp", {}).get("source_port", ""),
                 packet_stats.get("tcp", {}).get("destination_port", ""),
-                packet_stats.get("tcp", {}).get("flags", ""),
+                packet_stats.get("tcp", {}).get("00010000", "")[2:],
                 packet_stats.get("icmp", {}).get("type", ""),
                 packet_stats.get("icmp", {}).get("code", ""),
+                packet_stats.get("udp", {}).get("source_port", ""),
+                packet_stats.get("udp", {}).get("destination_port", ""),
             ]
             self.stats_tree.insert("", "end", text=row[0], values=row[1:])
 
         # Resize columns to fit content
         for i in range(len(row)):
-            self.stats_tree.column(i, width=tk.FIXED)
-            self.stats_tree.heading(i, text=row[i])
+            self.stats_tree.column(i, width=100)
+            # self.stats_tree.heading(i, text=row[i])
 
     def run(self, *args):
         print("Running GUI interface...", args)
